ccb4e7a16edb6a8c4815b35e14e3ddf61129073e
prajanshrestha <58924617+prajanshrestha@users.noreply.github.com>
2021-05-08 11:57:42 +0545

    updated

--- GoFashion/GoFashion/urls.py
+++ GoFashion/GoFashion/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('store.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
